139b4cc3be16c632fbe895e946d9f96eb163dc4b
rebornf <you@example.com>
2020-11-04 17:26:08 +0800

    update

---
+++ cloud-consumer-order80/src/main/java/com/cloud/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,12 @@
+package com.cloud.springcloud.config; /**
+ * Created with IntelliJ IDEA By fty on 2020/11/3
+ */
+
+/**
+ * @Author fty
+ * @Description TODO
+ * @Date 2020/11/3 22:37
+ * @Version V1.0
+ **/
+public class ApplicationContextConfig {
+}
---
+++ cloud-consumer-order80/src/main/java/com/cloud/springcloud/controller/OrderController.java
@@ -0,0 +1,12 @@
+package com.cloud.springcloud.controller; /**
+ * Created with IntelliJ IDEA By fty on 2020/11/3
+ */
+
+/**
+ * @Author fty
+ * @Description TODO
+ * @Date 2020/11/3 22:17
+ * @Version V1.0
+ **/
+public class OrderController {
+}
